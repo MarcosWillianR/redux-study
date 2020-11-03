39845bb48804745b4c652312f395a2f37cbbadba
Marcos Willian <markusuuuu@gmail.com>
2020-11-03 09:02:53 -0300

    code style configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
-const App: React.FC = () => {
-  return (
-    <h1>initial commit, clean default CRA archives.</h1>
-  )
-}
+const App: React.FC = () => (
+  <h1>initial commit, clean default CRA archives.</h1>
+);
 
 export default App;
